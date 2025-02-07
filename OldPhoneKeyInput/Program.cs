779268e5e11706e893fbbee8de66350b2f8e6d2d
ShineWaiTun <shinewaitun49@gmail.com>
2025-02-07 23:45:35 +0630

    first commit

---
+++ OldPhoneKeyInput/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace OldPhoneKeyInput
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            string result = OldPhonePad("5555557777772*2#");
+            Console.WriteLine(result);
+        }
+
+        public static string OldPhonePad(string input)
+        {
+            if (string.IsNullOrEmpty(input))
+            {
+                return "";
+            }
+            Dictionary<char, string> keyList = new Dictionary<char, string>
+            {
+                {'1', "&'("},{'2', "ABC"}, {'3', "DEF"},
+                {'4', "GHI"}, {'5', "JKL"}, {'6', "MNO"},
+                {'7', "PQRS"}, {'8', "TUV"}, {'9', "WXYZ"},
+                {'0', " "}
+            };
+            List<(char code, int count)> repeatedCount = new List<(char code, int count)>();
+            char lastKey = input[0];
+            int count = 0;
+            int i = 0;
+            do
+            {
+                if (lastKey == input[i])
+                {
+                    count++;
+                }
+                else
+                {
+                    repeatedCount.Add((lastKey, count));
+                    lastKey = input[i];
+                    count = 1;
+                }
+                i++;
+            } while (input[i] != '#');
+
+            repeatedCount.Add((lastKey, count));
+
+            string resultString = "";
+            foreach (var item in repeatedCount)
+            {
+                foreach (var key in keyList)
+                {
+                    if (item.code == '*')
+                    {
+                        resultString = resultString.Remove(resultString.Length - item.count);
+                        break;
+                    }
+                    if (item.code == key.Key)
+                    {
+                        resultString += key.Value[item.count - 1];
+                    }
+                }
+            }
+            return resultString;
+        }
+    }
+}
---
+++ OldPhoneKeyInputTests/ProgramTests.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OldPhoneKeyInput;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OldPhoneKeyInput.Tests
+{
+    [TestClass()]
+    public class ProgramTests
+    {
+        [TestMethod()]
+        public void TestOldPhonePad_ValidInput_ReturnsCorrectOutput()
+        {
+            string input = "33#";
+            string expectedOutput = "E";
+
+            string result = Program.OldPhonePad(input);
+            Assert.AreEqual(expectedOutput, result);
+        }
+
+        [TestMethod()]
+        public void TestOldPhonePad_ValidInput_ReturnsEmptyString()
+        {
+            string input = "";
+            string expectedOutput = "";
+
+            string result = Program.OldPhonePad(input);
+            Assert.AreEqual(expectedOutput, result);
+        }
+
+        [TestMethod()]
+        public void TestOldPhonePad_ValidInputWithStar_ReturnsCorrectOutput()
+        {
+            string input = "227*#";
+            string expectedOutput = "B";
+
+            string result = Program.OldPhonePad(input);
+            Assert.AreEqual(expectedOutput, result);
+        }
+
+        [TestMethod()]
+        public void TestOldPhonePad_ValidInputWithMultipleStar_ReturnsCorrectOutput()
+        {
+            string input = "227**2#";
+            string expectedOutput = "A";
+
+            string result = Program.OldPhonePad(input);
+            Assert.AreEqual(expectedOutput, result);
+        }
+
+        [TestMethod()]
+        public void TestOldPhonePad_ValidInputWithSpace_ReturnsCorrectOutput()
+        {
+            string input = "4433555 555666#";
+            string expectedOutput = "HELLO";
+
+            string result = Program.OldPhonePad(input);
+            Assert.AreEqual(expectedOutput, result);
+        }
+
+        [TestMethod()]
+        public void TestOldPhonePad_ValidInputWithSpaceAndStar_ReturnsCorrectOutput()
+        {
+            string input = "8 88777444666*664#";
+            string expectedOutput = "TURING";
+
+            string result = Program.OldPhonePad(input);
+            Assert.AreEqual(expectedOutput, result);
+        }
+    }
+}
